8d843ee03d0d39f191da42907f1b9549314a5488
DRSUCCESS <a4surprise@gmail.com>
2023-09-19 04:08:19 -0700

    Task 0

--- myfunctions.c
+++ myfunctions.c
@@ -1,65 +1,50 @@
 #include "main.h"
 
+/**
+ * _printf - function that produces output according to a format
+ * @format: a character string for the function
+ * Return: Always 0
+ */
+
 int _printf(const char *format, ...)
 {
-        int myChar = 0;
-        va_list args;
-        if (format == NULL)
-        {
-                return (-1);
-        }
+	int myChar = 0;
+	va_list args;
 
-        va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-        while (*format)
-        {
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        myChar++;
-                }
-                else
-                {
-                        format++;
-                        if (*format == '\0')
-                        {
-                                break;
-                        }
-                        else if (*format == '%')
-                        {
-                                write(1, format, 1);
-                                myChar++;
-                        }
-                        else if (*format == 'c')
-                        {
-                                char c = va_arg(args, int);
-                                write(1, &c, 1);
-                                myChar++;
-                        }
-                        else if (*format == 's')
-                        {
-                                char *str = va_arg(args, char*);
-                                int str_len = 0;
-                                while (str[str_len] != '\0')
-                                {
-                                        str_len++;
-                                }
-                                write(1, str, str_len);
-                                myChar += str_len;
-                        }
-                }
-                format++;
-        }
-        va_end(args);
-        return (myChar);
+	va_start(args, format);
+
+	while (*format && *format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			myChar++;
+		}
+		else
+		{
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				myChar++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				myChar += str_len;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (myChar);
 }
-
-/* Main function to test all the characters to check for task 0 (c,s,%) */
-
-    // int main()
-    // {
-    //     _printf("Leo\n");
-    //     _printf("%c\n", 'V');
-    //     _printf("%s\n", "String");
-    //     _printf("%%\n");
-    // }
